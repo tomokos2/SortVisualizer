671936d71b290885dd81d249ab0cc966e3dd0cf5
tomokos2 <tomokos2@illinois.edu>
2020-06-08 02:13:43 -0500

    Fully implemented merge sort.  Strangely enough, the display bugs don't occur on merge as they do on bubble and insertion.

--- SortVisualizer/src/application/SortingAlgs/MergeSort.java
+++ SortVisualizer/src/application/SortingAlgs/MergeSort.java
@@ -12,23 +12,11 @@ public class MergeSort extends Algorithm {
 	@Override
 	void sort() {
 		System.out.println("Sorting merge");
-		for (int i = 0; i < arr.length; i++) {
-			System.out.print(arr[i] + ", ");
-		}
-		
-		System.out.println();
-		
 		mergeSort(arr, 0, arr.length);
-		
-		for (int i = 0; i < arr.length; i++) {
-			System.out.print(arr[i] + ", ");
-		}
-		
-		System.out.println();
 	}
 	
 	private void mergeSort(int[] currArray, int left, int right) {
-		
+		// Periodically check for user interruption
 		if (isPaused) pause();
 		if (wasStopRequested) return;
 		
@@ -54,12 +42,18 @@ public class MergeSort extends Algorithm {
 	}
 	
 	
-	// Merges arr[left:mid] and arr[mid:right] with assumption that those sections are both sorted
+	/** Merges array[left:mid] and array[mid:right] with assumption that those sections are both sorted
+	 * @param currentArray the array to sort
+	 * @param left index of the left bound, inclusive
+	 * @param mid index of the middle of the array, between the two sorted sections
+	 * @param right index of the right bound, inclusive
+	 */
 	private void merge(int[] currentArray, int left, int mid, int right) {
-		// TODO: possibly eliminate currentArray and use arr instead
 		
+		// Dynamically created array of the new bar heights
 		double[] updatedHeights = new double[right - left];
 		
+		// 
 		int[] a = new int[mid - left];
 		int[] b = new int[right - mid];
 		
@@ -77,23 +71,11 @@ public class MergeSort extends Algorithm {
 			b[bIndex] = currentArray[i];
 			bIndex++;
 		}
-		
-		System.out.println("a");
-		for (int i = 0; i < a.length; i++) {
-			System.out.print(a[i] + ", ");
-		}
-		System.out.println();
-		System.out.println("b");
-		for (int i = 0; i < b.length; i++) {
-			System.out.print(b[i] + ", ");
-		}
-		System.out.println();
 	
 		aIndex = 0;
 		bIndex= 0;
 		
 		for (int i = left; i < right; i++) {
-			System.out.println("left " + left + " " + "mid " + mid + " " + "right " + right + " bindex " + bIndex + " aindex " + aIndex);
 			
 			paintCurrentComparison(left, mid, aIndex, a.length, bIndex, b.length);
 			
@@ -104,7 +86,7 @@ public class MergeSort extends Algorithm {
 			
 			if (aIndex == a.length) {
 				// No more elements in a, so fill the rest with b
-				
+				// Get the height of that element from the graph and store it in the array
 				updatedHeights[i - left] = ((Rectangle) graphArea.getChildren().get(bIndex + mid)).getHeight();
 				currentArray[i] = b[bIndex];
 				bIndex++;
@@ -117,13 +99,14 @@ public class MergeSort extends Algorithm {
 				aIndex++;
 				
 			} else if (a[aIndex] <= b[bIndex]) {
-				// Fill with minimum of a and b
+				// A is smaller, so fill with a
 				
 				updatedHeights[i - left] = ((Rectangle) graphArea.getChildren().get(aIndex + left)).getHeight();
 				currentArray[i] = a[aIndex];
 				aIndex++;
 				
 			} else {	
+				// B is smaller, so fill with b
 				
 				updatedHeights[i - left] = ((Rectangle) graphArea.getChildren().get(bIndex + mid)).getHeight();
 				currentArray[i] = b[bIndex];
@@ -132,20 +115,13 @@ public class MergeSort extends Algorithm {
 
 		}
 		
-		System.out.println("heights");
-		for (int i = 0; i < updatedHeights.length; i++) {
-
-			System.out.print(updatedHeights[i] + ", ");
-		}
-//		for (int i = left; i < right; i++) {
-//			((Rectangle) graphArea.getChildren().get(i)).setHeight(updatedHeights[left + i]);
-//		}
-		
-		
+		// Create the newly merged bars on the graph
+		updateGraph(updatedHeights, left, right);
 		
 	}
 	
 	private void paintCurrentComparison(int left, int mid, int aIndex, int aLen, int bIndex, int bLen) {
+		// Only paint if the indices have not yet reached past the array length
 		if (aIndex != aLen) {
 			paintBar(aIndex + left, SELECTED_BAR_COLOR);
 		}
@@ -158,12 +134,30 @@ public class MergeSort extends Algorithm {
 	}
 	
 	private void unPaintCurrentComparison(int left, int mid, int aIndex, int aLen, int bIndex, int bLen) {
+		// Only unpaint if the indices have not yet reached past the array length
 		if (aIndex != aLen) {
 			resetBarColor(aIndex + left);
 		}
 		
 		if (bIndex != bLen) {
 			resetBarColor(bIndex + mid);
+		}
+	}
+	
+	/**
+	 * Draws each bar on the graph with the height specified in the array
+	 * @param updatedHeights array of calculated heights for each bar
+	 * @param left is an int of left bound
+	 * @param right is an int of right bound
+	 */
+	private void updateGraph(double[] updatedHeights, int left, int right) {
+	
+		for (int i = left; i < right; i++) {
+			paintBar(i, SELECTED_BAR_COLOR);
+			((Rectangle) graphArea.getChildren().get(i)).setHeight(updatedHeights[i - left]);
+			
+			sleep();
+			resetBarColor(i);
 		}
 	}
 
